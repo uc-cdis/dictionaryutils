fad9725b4ce5b7806a8c8fb74eba1ba466003b63
Zac Flamig <Zac@WeatherWary.com>
2018-07-04 03:28:22 -0500

    install...

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,7 @@ cd ..
 DICTCOMMIT=`git rev-parse HEAD` && echo "DICTCOMMIT=\"${DICTCOMMIT}\"" >dictionaryutils/dictionaryutils/version_data.py
 DICTVERSION=`git describe --always --tags` && echo "DICTVERSION=\"${DICTVERSION}\"" >>dictionaryutils/dictionaryutils/version_data.py
 cd dictionaryutils
+python setup.py install
 pip install -r dev-requirements.txt
 nosetests -v
 python bin/dump_schema.py
