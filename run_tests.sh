8fe0f2b498701a595f41d115522ff24d4b2f570e
Zac Flamig <Zac@WeatherWary.com>
2018-07-04 03:39:14 -0500

    force uninstall

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,7 @@ cd ..
 DICTCOMMIT=`git rev-parse HEAD` && echo "DICTCOMMIT=\"${DICTCOMMIT}\"" >dictionaryutils/dictionaryutils/version_data.py
 DICTVERSION=`git describe --always --tags` && echo "DICTVERSION=\"${DICTVERSION}\"" >>dictionaryutils/dictionaryutils/version_data.py
 cd dictionaryutils
-pip uninstall dictionaryutils
+pip uninstall -y dictionaryutils
 python setup.py install --force
 pip install -r dev-requirements.txt
 # always use this version of dictionaryutils...
