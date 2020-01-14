2de109de7eb9273cf10f0d07eb678b0244aaeb11
Pauline <ribeyre@uchicago.edu>
2020-01-14 15:50:35 -0600

    pyyaml 5.3

--- dictionaryutils/__init__.py
+++ dictionaryutils/__init__.py
@@ -35,6 +35,7 @@ def visit_directory(path):
 
 def load_yaml(name):
     """Return contents of yaml file as dict"""
+    print(name)
     with open(name, "r") as f:
         return yaml.safe_load(f)
 
